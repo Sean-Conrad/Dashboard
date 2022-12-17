5b148d20cf89208c049e637fcb6bcc37ac686168
Sean-Conrad <98624752+Sean-Conrad@users.noreply.github.com>
2022-12-17 18:24:21 +0800

    handle log-in post request

--- models/userModel.js
+++ models/userModel.js
@@ -33,7 +33,7 @@ userSchema.statics.signup = async function(email, password) {
         throw Error("Password is not strong enough");
     }
 
-    const exists = await this.findOne({ email }) //use this to refer to this model.
+    const exists = await this.findOne({ email }) //use "this" to refer to this model.
 
     if(exists){
         throw Error('email already in use')
@@ -46,6 +46,29 @@ userSchema.statics.signup = async function(email, password) {
 
     return user;
 }
+
+//static log in method
+userSchema.statics.login = async function(email, password){
+    
+    //validation
+    if (!email || !password){
+        throw Error("All fields must be filled");
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match) {
+        throw Error('Incorrect password')
+    }
+    return user;
+}
+
 
 
 module.exports = mongoose.model('User', userSchema)
