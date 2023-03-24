15d9843a9fb905a6b95a16a9925de9f5889994c7
Sean-Conrad <98624752+Sean-Conrad@users.noreply.github.com>
2023-03-24 15:27:26 -0500

    clean whitespace

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -4,14 +4,11 @@ export const useLogout = () => {
     const {dispatch} = useAuthContext()
 
     const logout = () => {
-
-
         //remove User from storage
         localStorage.removeItem('user')
 
         //dispatch logout action
         dispatch({type: 'LOGOUT'})
     }
-
     return {logout}
 }
